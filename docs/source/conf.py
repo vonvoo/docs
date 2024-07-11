35da85faf8c0276896ae27f63ba79e7e898b8bdc
Vonvoo <104660502+vonvoo@users.noreply.github.com>
2024-07-11 18:28:46 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Vonvoo'
+copyright = '2024, Vonvoo'
+author = 'Vonvoo'
 
 release = '0.1'
-version = '0.1.0'
+version = '0.1.2'
 
 # -- General configuration
 
