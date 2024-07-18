1324062cecb6043c685a07aea5579ca6a1cbad31
Vonvoo <104660502+vonvoo@users.noreply.github.com>
2024-07-18 09:44:32 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,5 +33,21 @@ html_theme = 'sphinx_rtd_theme'
 html_logo = 'images/branding/logo_vonvoo.png'
 html_favicon = 'images/branding/favicon.ico'
 
+html_theme_options = {
+    #'analytics_id': 'UA-xxx',  #  Provided by Google in your dashboard
+    'analytics_anonymize_ip': False,
+    'logo_only': True,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'CadetBlue',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
